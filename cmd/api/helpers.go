e5fe1cb59b4bdf717370056b3166bce50c8dc8b3
Ramiz Ahmed <ramizahmedar@gmail.com>
2021-08-28 15:25:20 +0500

    Indent terminal json res

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -19,7 +19,7 @@ func (app *application) readIdParam(params httprouter.Params) (int, error) {
 }
 
 func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
-	js, err := json.Marshal(data)
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
